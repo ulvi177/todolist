78e28615b9c7344744cc4b7057e70b96dcde7a1d
ulvi177 <ulvi-abdullayev@internet.ru>
2025-07-29 21:09:00 +0000

    todo

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 "use strict";
 $(document).ready(function () {
 
-});
+    
+        let input = $("input");
+        let value = input.value.trim();
+        if (value !== "") {
+            let div = $("div");
+            div.className = "todo-item";
+            div.textContent = value;
+            $("#list").appendChild(div);
+        
+        }
+    });
+        
